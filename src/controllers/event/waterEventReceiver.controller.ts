bef602f487165f3a72699b238833767c517de82d
Almerindo Rehem <almerindo.rehem@gmail.com>
2022-08-13 18:21:34 -0300

    chore: modify eventReceiver to async await

--- src/controllers/event/waterEventReceiver.controller.ts
+++ src/controllers/event/waterEventReceiver.controller.ts
@@ -5,13 +5,12 @@ const URL_API = 'http://200.100.0.114:3000';
 
 // eslint-disable-next-line import/prefer-default-export
 export const eventReceiver = async (izarData: IIzar): Promise<void> => {
-  axios
-      .post(`${URL_API}/event/new`, izarData)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  try {
+    const response = await axios.post(`${URL_API}/event/new`, izarData);
+    console.info(response.data);
+    
+  } catch (error: any) {
+    console.error(error.message)
+  }
   //axios
 };
